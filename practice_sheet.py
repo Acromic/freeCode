f730b8d105d7bad8d88ddb6f278000465a9fa865
Tony <acromic22@yahoo.com>
2022-08-17 17:59:27 -0700

    update

---
+++ practice_sheet.py
@@ -0,0 +1,17 @@
+# Intermediate Expressions (order)
+# Parenthesis
+# Power
+# Multiplication/Division
+# Addition/Subtraction
+# Left to right
+
+# exercise
+# width = 15
+# height = 12
+# print(height/3)
+
+
+for i in range(1,5,2):
+    pass
+print(i)
+
